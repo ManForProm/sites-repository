1716041880d01d0dfa2d825c0bd75cde5f5363b8
Govs_E <manforprom@gmail.com>
2024-10-21 14:23:23 +0300

    Added footer and adaptive layouts

--- public/site-safari/js/main.js
+++ public/site-safari/js/main.js
@@ -26,10 +26,11 @@ $(function(){
 			}
 		}
 	});
-
-
     $('.gallery__slider').slick({
         prevArrow: '<button type="button" class="slick-btn slick-prev"> <img src="./assets/images/arrow-left.svg" alt="" > </button>',
         nextArrow: '<button type="button" class="slick-btn slick-next"> <img src="./assets/images/arrow-right.svg" alt="" > </button>'
     });
+	$('.menu__btn').on('click', function() {
+		$('.menu__list').toggleClass('menu__list--active')
+	});
 });
